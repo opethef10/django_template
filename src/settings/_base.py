802a4860e9755609a49366684ab1487c5cb968c5
Onur Arıkan <onur93arikan@gmail.com>
2025-06-01 19:01:01 +0200

    Change the db file name

--- src/settings/_base.py
+++ src/settings/_base.py
@@ -92,7 +92,7 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / f'{PROJECT_SLUG}.sqlite3',
     }
 }
 
