f02475dbe628cf40172a2e5af19111043207440b
Onur Arıkan <onur93arikan@gmail.com>
2026-03-29 11:29:28 +0200

    Add custom signup form

--- src/forms.py
+++ src/forms.py
@@ -1,3 +1,4 @@
+from allauth.account.forms import SignupForm
 from django import forms
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
@@ -15,3 +16,15 @@ class ContactForm(forms.Form):
         if getattr(settings, 'RECAPTCHA_ENABLED', False):
             # Only add the ReCaptchaField if RECAPTCHA_ENABLED is True
             self.fields['captcha'] = ReCaptchaField(label=_("Captcha"))
+
+
+class CustomSignupForm(SignupForm):
+    first_name = forms.CharField(max_length=30, label=_("Name"))
+    last_name = forms.CharField(max_length=30, label=_("Surname"))
+
+    def save(self, request):
+        user = super(CustomSignupForm, self).save(request)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.save()
+        return user
