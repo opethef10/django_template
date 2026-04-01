2b5c8f0b36321bc0dccb69e45767d400684cd9c3
Onur Arıkan <onur93arikan@gmail.com>
2026-04-01 13:14:52 +0200

    Bump version to 0.7.0

--- src/menus.py
+++ src/menus.py
@@ -1,7 +1,47 @@
 from django.utils.translation import gettext_lazy as _
 
 NAVBAR_MENUS = [
-
+    {
+        "title": _("Docs"),
+        "items": [
+            {
+                "label": _("Home"),
+                "url_name": "example:index",
+                "args": [],
+                "kwargs": {},
+            },
+            {
+                "label": _("Setup"),
+                "url_name": "example:setup",
+                "args": [],
+                "kwargs": {},
+            },
+            {
+                "label": _("Structure"),
+                "url_name": "example:structure",
+                "args": [],
+                "kwargs": {},
+            },
+            {
+                "label": _("Settings"),
+                "url_name": "example:settings",
+                "args": [],
+                "kwargs": {},
+            },
+            {
+                "label": _("Features"),
+                "url_name": "example:features",
+                "args": [],
+                "kwargs": {},
+            },
+            {
+                "label": _("Deployment"),
+                "url_name": "example:deployment",
+                "args": [],
+                "kwargs": {},
+            },
+        ]
+    },
 ]
 
 FOOTER_LINKS = [
@@ -10,14 +50,16 @@ FOOTER_LINKS = [
         "url_name": "contact",
     },
     {
-        "label": _("Changelog"),
-        "url_name": "django.contrib.flatpages.views.flatpage",
-        "kwargs": {"url": "changelog/"},
+        "label": _("Documentation"),
+        "url_name": "example:index",
     },
     {
-        "label": _("Frequently Asked Questions"),
-        "url_name": "django.contrib.flatpages.views.flatpage",
-        "kwargs": {"url": "faq/"},
+        "label": _("Setup Guide"),
+        "url_name": "example:setup",
+    },
+    {
+        "label": _("PythonAnywhere"),
+        "url_name": "example:pythonanywhere",
     },
 ]
 
