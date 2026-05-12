f986687b786772833abc1a089f5b1441a1b82067
Onur Arıkan <onur93arikan@gmail.com>
2026-05-12 13:25:28 +0200

    Add migrate command

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+python manage.py migrate --noinput
+
 python manage.py collectstatic --noinput
 
 exec gunicorn --bind "0.0.0.0:8000" --workers 2 --access-logfile - src.wsgi:application
